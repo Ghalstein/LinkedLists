92effbfb192db32e3b5480a1051320fbefbf38a6
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-27 17:05:42 -0400

    ListNode toString unit tested

--- ListNode.java
+++ ListNode.java
@@ -18,19 +18,28 @@ public class ListNode<T> {
 		}
 	}
 
-	public String toString(ListNode list) {
+	public String toString() {
 		String str = "[";
-		
 		// adding current data in the ListNode
-		str += list.data;
-		ListNode curr = list.next;
+		str += this.data;
+		ListNode<T> curr = this.next;
 		// iteration of listNode
 		while (curr != null) {
-			str += curr.data + " "
+			str += " " + curr.data;
 			curr = curr.next;
 		}
 		str += "]";
 
 		return str;
+	}
+
+	public static void main(String[] args) {
+
+		//unit testing list node
+		ListNode<Integer> list = new ListNode<>();
+		list.data = 1;
+		list.next = new ListNode<>();
+		list.next.data = 2;
+		System.out.println(list.toString());
 	}
 }
