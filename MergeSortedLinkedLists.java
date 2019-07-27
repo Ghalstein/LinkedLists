a23d6d6d4dae6b5b23dc21732a5a49001175c6aa
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-27 17:10:20 -0400

    unit tested the randomSortedListGen for the Merge class

--- MergeSortedLinkedLists.java
+++ MergeSortedLinkedLists.java
@@ -1,16 +1,15 @@
 public class MergeSortedLinkedLists {
 
-
 	// random Sorted listNode generator
-	public static ListNode randomSortedListGen(int n, int init) {
+	private static ListNode randomSortedListGen(int n, int init) {
 		ListNode<Integer> list = new ListNode<>();
-		ListNode curr = list;
+		ListNode<Integer> curr = list;
 		int j = init;
 		for (int i = 0; i < n; ++i) {
 			if (i < n -1) {
 				int rand = j + 1 + (int)(Math.random() * 10); 
 				curr.data = rand;
-				curr.next = new listNode();
+				curr.next = new ListNode<>();
 				curr = curr.next;
 				j = rand;
 			}
@@ -25,5 +24,7 @@ public class MergeSortedLinkedLists {
 	public static void main(String[] args) {
 		ListNode list1 = randomSortedListGen(5, 3);
 		ListNode list2 = randomSortedListGen(5, 5);
+		System.out.println("list1: " + list1.toString());
+		System.out.println("list2: " + list2.toString());
 	}
 }
