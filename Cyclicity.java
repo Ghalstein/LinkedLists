52e4112f72ab67f0b290359df11739ebc18bcc8c
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-29 19:09:26 -0400

    added the isCyclic function

--- Cyclicity.java
+++ Cyclicity.java
@@ -1,11 +1,38 @@
 public class Cyclicity {
 
-	public static boolean isCyclic() {
+	public static boolean isCyclic(ListNode<Integer> list) {
+		ListNode<Integer> fast = list.next;
+		ListNode<Integer> slow = list;
 
+		// iterates through list
+		while (fast != null) {
+			// if they are equal then it is cyclic 
+			if (fast == slow) {
+				return true;
+			}
+			// if fast.next is not null then we can incrmeent it two
+			else if (fast.next != null){
+				fast = fast.next.next;
+				slow = slow.next;
+			}
+			else {
+				return false;
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
 
-		ListNode<Integer> lsit = ListNode.randGen();
+		ListNode<Integer> list = ListNode.randGen(10);
+		System.out.println("Non-cyclic: " + list);
+		System.out.println("Tests " + isCyclic(list));
+
+		System.out.println();
+		ListNode<Integer> cyclic = new ListNode<Integer>();
+		cyclic.next = new ListNode<Integer>();
+		cyclic = cyclic.next.next;
+		System.out.println("Cyclic: " + cyclic);
+		System.out.println("Tests " + isCyclic(cyclic));
 	}
 }
