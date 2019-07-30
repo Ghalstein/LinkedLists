b6412db48133d25620372a61c1b3a924f4d04087
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-30 14:50:23 -0400

    finsihed the remove fucntion

--- RemoveSortedDuplicates.java
+++ RemoveSortedDuplicates.java
@@ -3,11 +3,15 @@ import java.util.*;
 public class RemoveSortedDuplicates {
 
 	public static void remove(ListNode<Integer> list) {
-		
+		// tracer
+		ListNode<Integer> curr = list;
 
-		while (list.next != null) {
-			if (list.data == list.next.data) {
-				list.next = list.next.next;
+		while (curr.next != null) {
+			if (curr.data == curr.next.data) {
+				curr.next = curr.next.next;
+			}
+			else {
+				curr = curr.next;
 			}
 		}
 	}
@@ -17,6 +21,8 @@ public class RemoveSortedDuplicates {
 		testList.data = 1;
 		testList.next = new ListNode<>();
 		testList.next.data = 1;
+		testList.next.next = new ListNode<>();
+		testList.next.next.data = 1;
 		remove(testList);
 		System.out.println(testList);
 	}
