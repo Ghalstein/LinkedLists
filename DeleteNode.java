3dbc4c6bea3f8f328ca5794d8531a32d277928ae
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-30 07:38:46 -0400

    made the function

---
+++ DeleteNode.java
@@ -0,0 +1,12 @@
+public class DeleteNode {
+
+	public static delete(ListNode<Integer> node) {
+		node.data = node.next.data;
+		node.next = ndoe.next.next;
+	}
+
+	public static void main(String[] args) {
+
+
+	}
+}
