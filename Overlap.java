c32169350e0dcab283bd512e2b795f2edec92124
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-29 20:26:30 -0400

    made the overlap function

---
+++ Overlap.java
@@ -0,0 +1,29 @@
+public class Overlap {
+
+	public static boolean isOverlap(ListNode<Integer> l1, ListNode<Integer> l2) {
+
+
+		while (l1.next != null || l2.next != null) {
+			// testing for each list if next is null and incrementing if not
+			if (l1.next != null) {
+				l1 = l1.next;
+			}
+
+			if (l2.next != null) {
+				l2 = l2.next;
+			}
+		}
+
+		if (l2 == l1) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+	public static void main(String[] args) {
+		
+	}
+}
