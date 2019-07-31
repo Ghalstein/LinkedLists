7f8b71ae3ca3e91fd6ffa94d8d8ac88094b00a5d
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-31 07:46:16 -0400

    function is returning correctly

--- EvenOddMerge.java
+++ EvenOddMerge.java
@@ -1,16 +1,28 @@
 public class EvenOddMerge {
 
-	public static void merge(ListNode<Integer> list) {
+	public static ListNode<Integer> merge(ListNode<Integer> list) {
 		ListNode<Integer> oddHead = list.next;
 		ListNode<Integer> oddCurr = list.next;
+		ListNode<Integer> head = list;
 		ListNode<Integer> evenCurr = list;
 
-		while (oddCurr.next != null && evenCurr.next != null) {
-			
+		// making sure not at the end of the list for an even or odd case
+		while (oddCurr != null && evenCurr != null) {
+			if (oddCurr.next != null && evenCurr.next != null) {
+				evenCurr.next = evenCurr.next.next;
+				oddCurr.next = oddCurr.next.next;
+				evenCurr = evenCurr.next;
+				oddCurr = oddCurr.next;
+			}
 		}
+		// links the even and odd lists together
+		evenCurr.next = oddHead;
+		return head;
 	}
 
 	public static void main(String[] args) {
-
+		ListNode <Integer> list = ListNode.randGen(10);
+		System.out.println("Before: " + list);
+		System.out.println("After: " + merge(list));
 	}
 }
