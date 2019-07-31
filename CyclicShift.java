233d98add2cd823f5c02a5076f943e5412d12e2e
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-30 20:02:31 -0400

    turns out the function was working properly but the pointer was not changing

--- CyclicShift.java
+++ CyclicShift.java
@@ -1,7 +1,7 @@
 public class CyclicShift {
 
 	// counts to the number n and then makes that node the end and the one infront of it the start
-	public static void shift(ListNode<Integer> list, int n) {
+	public static ListNode<Integer> shift(ListNode<Integer> list, int n) {
 		ListNode<Integer> head = list;
 		ListNode<Integer> curr = list;
 		// keeps track of how deep in the list 
@@ -13,18 +13,20 @@ public class CyclicShift {
 		ListNode<Integer> newHead = curr.next;
 		// setting the curr node to the end of the list 
 		curr.next = null;
+		list = newHead;
+		curr = newHead;
 		// finding the old end of the list to connect it to the old head of the list
-		while (newHead.next != null) {
-			newHead = newHead.next;
+		while (curr.next != null) {
+			curr = curr.next;
 		}
 		// setting the old end of the list to the old head of the list
-		newHead.next = head;
+		curr.next = head;
+		return list;
 	}
 
 	public static void main(String[] args) {
 		ListNode<Integer> list = ListNode.randGen(5);
 		System.out.println("Before: " + list);
-		shift(list, 3);
-		System.out.println("After: " + list);
+		System.out.println("After: " + shift(list, 3));
 	}
 }
