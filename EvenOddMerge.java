1544a228e8b70b784554649b7827151dbd71d3db
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-30 20:13:13 -0400

    set up the foundation for the merge function just have fill some logic in

---
+++ EvenOddMerge.java
@@ -0,0 +1,16 @@
+public class EvenOddMerge {
+
+	public static void merge(ListNode<Integer> list) {
+		ListNode<Integer> oddHead = list.next;
+		ListNode<Integer> oddCurr = list.next;
+		ListNode<Integer> evenCurr = list;
+
+		while (oddCurr.next != null && evenCurr.next != null) {
+			
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
