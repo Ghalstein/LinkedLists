675f36bdf1c2a25a974f4a285ea7b5c73f9b3236
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-20 00:07:37 -0400

    got it working for everything except first nodes, have to make small exception for when 1 is selected

--- DeleteMiddleNode.java
+++ DeleteMiddleNode.java
@@ -1,7 +1,43 @@
+/*
+Attemp to only delete a middle node
+Not meant for first nodes, but will add exception
+*/
+
 public class DeleteMiddleNode {
 
 
+	public static int delete(ListNode<Integer> list, int num) {
+		ListNode<Integer> curr = list.next;
+		ListNode<Integer> prev = list;
+		int count = 2;
+		if (count > num) {
+			return -1;
+		}
+
+		if ()
+		while (curr != null) {
+			if (count == num) {
+				int data = curr.data;
+				prev.next = curr.next;
+				return data;
+			}
+			else {
+				++count;
+				prev = prev.next;
+				curr = curr.next;
+			}
+		}
+		return -1;
+	}
+
 	public static void main(String[] args) {
-		
+		ListNode<Integer> list1 = ListNode.randGen(5);
+		ListNode<Integer> list2 = ListNode.randGen(5);
+		System.out.println("before list1: " + list1);
+		System.out.println("before list2: " + list2);
+		System.out.println(delete(list1, 4));
+		System.out.println(delete(list2, 5));
+		System.out.println("after list1: " + list1);
+		System.out.println("after list2: " + list2);
 	}
 }
