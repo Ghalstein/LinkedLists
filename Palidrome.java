b9873df0282966d536b134018945b6e77df7b8a7
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-21 12:48:46 -0400

    working on Palidrome validator

--- Palidrome.java
+++ Palidrome.java
@@ -1,5 +1,14 @@
+/*
+check if list node is palidromic by making a hashmap as we go through it
+*/
+
+import java.util.*;
+
 public class Palidrome {
-	
+
+	public static boolean valid(ListNode<Integer> list) {
+
+	}
 
 	public static void main(String[] args) {
 
