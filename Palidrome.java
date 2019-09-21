79ff4c74c66bfbf638d15d6fb412322dda8c22ec
Garrett Halstein <garrett.halstein@emory.edu>
2019-09-21 13:30:22 -0400

    palidrome detector works

--- Palidrome.java
+++ Palidrome.java
@@ -14,19 +14,59 @@ public class Palidrome {
 			++count;
 			curr = curr.next;
 		}
+		return count;
+	}
+
+	private static boolean isOdd(int num) {
+		if (num % 2 == 1) {
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 	// tests if the palidrome is valid or not
 	public static boolean valid(ListNode<Integer> list) {
 		// key is for the node in the list and vsalur is the occurences
-		// HashMap<Integer, Integer> hash = HashMap<>();
-		ListNod<Integer> curr = list;
+		ListNode<Integer> curr = list;
+		int size = size(list);
 
+		// iterating through the list nodes and tracking the order
+		int count = 0;
+		int[] firstHalf = new int[size / 2];
+		while (count < size / 2) {
+			firstHalf[count++] = curr.data;
+			curr = curr.next;
+		}
 
+		if (isOdd(size)) {
+			curr = curr.next;
+		}
+
+		while (curr != null) {
+			if (firstHalf[--count] != curr.data) {
+				// System.out.println(firstHalf[count + 1]);
+				// System.out.println(curr.data);
+				return false;
+			}
+			curr = curr.next;
+		}
+		return true;
 
 	}
 
 	public static void main(String[] args) {
-
+		ListNode<Integer> list = new ListNode<>();
+		list.data = 1;
+		list.next = new ListNode<>();
+		list.next.data = 2;
+		list.next.next = new ListNode<>();
+		list.next.next.data = 2;
+		list.next.next.next = new ListNode<>();
+		list.next.next.next.data = 2;
+		list.next.next.next.next = new ListNode<>();
+		list.next.next.next.next.data = 1;
+		System.out.println(valid(list));
 	}
 }
